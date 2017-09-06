6e8421e8380860fc57d9a39af961d4b0d8dd7070
Kasia Grabowska <kattie.grab@gmail.com>
2017-09-06 12:45:06 +0200

    finished styles

--- zadanie-rekrutacyjne/js/index.js
+++ zadanie-rekrutacyjne/js/index.js
@@ -1,5 +1,27 @@
+// $(document).ready(function() {
+// 	const carouselList = $("#carousel ul");
+	
+// 	setInterval(changeSlide, 5000);
+	
+// 	function changeSlide() { 
+// 		for (let i = 0; i < carouselList.length; i++) {
+// 			carouselList.animate({'marginLeft':-600}, 1000, moveFirstSlide);
+// 		}
+// 		carouselList.mouseenter(stopCarousel);
+// 	}	
+
+// 	function moveFirstSlide() {
+// 		const firstItem = carouselList.find("li:first");
+// 		const lastItem = carouselList.find("li:last");
+// 		lastItem.after(firstItem);
+// 		carouselList.css({marginLeft:0});
+// 	}
+
+// 	function stopCarousel() {
+// 		carouselList.stop();
+// 	};
+// })
+
 $(document).ready(function() {
-	const sliderItem = 
-	const theWidth = sliderItem.width();
 	
 })
