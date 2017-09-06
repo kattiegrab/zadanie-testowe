232ee98572304978ea6097bc85aa47e999d6296d
Kasia Grabowska <kattie.grab@gmail.com>
2017-09-06 19:57:30 +0200

    added slider

--- zadanie-rekrutacyjne/js/index.js
+++ zadanie-rekrutacyjne/js/index.js
@@ -1,27 +1,57 @@
-// $(document).ready(function() {
-// 	const carouselList = $("#carousel ul");
-	
-// 	setInterval(changeSlide, 5000);
-	
-// 	function changeSlide() { 
-// 		for (let i = 0; i < carouselList.length; i++) {
-// 			carouselList.animate({'marginLeft':-600}, 1000, moveFirstSlide);
-// 		}
-// 		carouselList.mouseenter(stopCarousel);
-// 	}	
-
-// 	function moveFirstSlide() {
-// 		const firstItem = carouselList.find("li:first");
-// 		const lastItem = carouselList.find("li:last");
-// 		lastItem.after(firstItem);
-// 		carouselList.css({marginLeft:0});
-// 	}
-
-// 	function stopCarousel() {
-// 		carouselList.stop();
-// 	};
-// })
 
 $(document).ready(function() {
-	
-})
+	$('#prev').on('click', function(){
+	    const $last = $('#carousel-list-1 li:last');
+	    $last.remove().css({ 'margin-left': '-600px' });
+	    $('#carousel-list-1 li:first').before($last);
+	    $last.animate({ 'margin-left': '0px' }, 1000); 
+	});
+	$('#prev').on('click', function(){
+	    const $last = $('#carousel-list-2 li:last');
+	    $last.remove().css({ 'margin-left': '-600px' });
+	    $('#carousel-list-2 li:first').before($last);
+	    $last.animate({ 'margin-left': '0px' }, 1000); 
+	});
+	$('#prev').on('click', function(){
+	    const $last = $('#carousel-list-3 li:last');
+	    $last.remove().css({ 'margin-left': '-600px' });
+	    $('#carousel-list-3 li:first').before($last);
+	    $last.animate({ 'margin-left': '0px' }, 1000); 
+	});
+	$('#prev').on('click', function(){
+	    const $last = $('#carousel-list-4 li:last');
+	    $last.remove().css({ 'margin-left': '-600px' });
+	    $('#carousel-list-4 li:first').before($last);
+	    $last.animate({ 'margin-left': '0px' }, 1000); 
+	});
+
+
+	$('#next').on('click', function(){
+	    var $first = $('#carousel-list-1 li:first');
+	    $first.animate({ 'margin-left': '-600px' }, 1000, function() {
+	        $first.remove().css({ 'margin-left': '0px' });
+	        $('#carousel-list-1 li:last').after($first);
+	    });
+	});
+	$('#next').on('click', function(){
+	    var $first = $('#carousel-list-2 li:first');
+	    $first.animate({ 'margin-left': '-600px' }, 1000, function() {
+	        $first.remove().css({ 'margin-left': '0px' });
+	        $('#carousel-list-2 li:last').after($first);
+	    });
+	});
+	$('#next').on('click', function(){
+	    var $first = $('#carousel-list-3 li:first');
+	    $first.animate({ 'margin-left': '-600px' }, 1000, function() {
+	        $first.remove().css({ 'margin-left': '0px' });
+	        $('#carousel-list-3 li:last').after($first);
+	    });
+	});
+	$('#next').on('click', function(){
+	    var $first = $('#carousel-list-4 li:first');
+	    $first.animate({ 'margin-left': '-600px' }, 1000, function() {
+	        $first.remove().css({ 'margin-left': '0px' });
+	        $('#carousel-list-4 li:last').after($first);
+	    });
+	});
+});
